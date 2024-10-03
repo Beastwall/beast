bcd88e05697a4e5aa99193e9eaf9f0965062b395
rasmi <abdelwadoudrasmi@gmail.com>
2024-10-03 20:44:52 +0200

    getting back to old code

--- src/main/java/com/beastwall/beastengine/BeastHtmlEngine.java
+++ src/main/java/com/beastwall/beastengine/BeastHtmlEngine.java
@@ -10,7 +10,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class BeastHtmlEngine extends BeastEngine {
 
@@ -99,9 +98,14 @@ public class BeastHtmlEngine extends BeastEngine {
                     break;
                 case TAG_PREFIX + "router":
                     String route = ((String) context.get(TAG_PREFIX + "path")).trim();
-                    for (Node child : element.childNodes().stream().filter(child -> child.nameIs("route") && route.equalsIgnoreCase(child.attr("path").trim())).collect(Collectors.toSet())) {
-                        result.append(renderComponent(child.attr("component").trim(), context, scopeIdentifier, child.attributes().hasKey("static"), resolvedVariables, engine));
-                    }
+                    element.childNodes().stream().filter(child -> child.nameIs("route") && route.equalsIgnoreCase(child.attr("path").trim())).forEachOrdered(child -> {
+                        //
+                        try {
+                            result.append(renderComponent(child.attr("component").trim(), context, scopeIdentifier, child.attributes().hasKey("static"), resolvedVariables, engine));
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
                     break;
                 case TAG_PREFIX + "component":
                     String componentName = element.attr("name").trim();
